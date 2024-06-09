2b8c83cd23b22bbcc1cb95812345cc594465fc0f
VictorTamayoBriones <victortamayo509@gmail.com>
2024-06-08 22:12:49 -0600

    My first class: Doctor

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Pedro Guadalupe Ramos";
+        myDoctor.showName();
+
+    }
+}
