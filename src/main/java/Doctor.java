24cf39f75feeb6c48d696df91e1981ee090100a3
VictorTamayoBriones <victortamayo509@gmail.com>
2024-06-08 23:37:28 -0600

    increment ID

--- src/main/java/Doctor.java
+++ src/main/java/Doctor.java
@@ -1,18 +1,26 @@
 public class Doctor {
+    static int global_id = 0;
     int id;
     String name;
     String speciality;
 
     Doctor(){
         System.out.println("----- Building a Doctor -----");
+        asignarId();
     }
 
     Doctor(String name){
         System.out.println("El nombre del doctor asignado es: " + name);
         this.name = name;
+        asignarId();
     }
 
     public void showName(){
         System.out.println(name);
+    }
+
+    private void asignarId(){
+        this.id = global_id;
+        global_id++;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,5 +8,12 @@ public class Main {
 
         Doctor secondDoctor = new Doctor("Víctor");
         secondDoctor.showName();
+
+        Doctor tercerDoctor = new Doctor("Jose Perez Perez");
+        tercerDoctor.showName();
+
+        System.out.println("FIRST DOCTOR ID: " + myDoctor.id);
+        System.out.println("Second Doctor ID: " + secondDoctor.id);
+        System.out.println("Tercer Doctor ID: " + tercerDoctor.id);
     }
 }
